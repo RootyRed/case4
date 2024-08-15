6a38a60bdffbca2ba2b052828ee6cef98fa2dd17
RootyRed <m_sergey.01@mail.ru>
2024-08-15 08:51:53 +0500

    Add files via upload

---
+++ lib/module/quickstart/inc-quickstart-dumping-devices.php
@@ -0,0 +1,74 @@
+<div class="container-con-superblock darkmode-txt">
+	<div class='container-con-superblock-emp'>
+	</div>
+	<h1>
+	Adding Your Games </h1>
+	<div class='container-con-superblock-div'>
+	</div>
+</div>
+<div class='container-con-block darkmode-block'>
+	<div class='container-con-wrapper'>
+		<div class="anchor-point" id="dumping_methods">
+		</div>
+		<div class='container-tx1-block darkmode-txt'>
+			<div class='container-emp-block'>
+			</div>
+			<h2>Game Dumping Methods</h2>
+		</div>
+		<div class='container-tx2-block darkmode-txt'>
+			<p class="download-desc">
+				 You can use both an original PlayStation 3 console with custom firmware or a PC with a compatible Blu-ray disc drive to dump your games.
+			</p>
+		</div>
+	</div>
+</div>
+<div class='device-con-container'>
+	<div class='device-con-outer-left'>
+		<div class='device-con-inner-a'>
+			<div class='performance-con-spec performance-ico-good'>
+				<div class='performance-ico-spec' style="background: url(/img/icons/buttons/good-h.png) center no-repeat; background-size:18px;">
+				</div>
+			</div>
+			<div class='device-con-gradient'>
+				<div class='device-con-graphic' style="background: url(/img/graphics/quickstart/ps3.png) center no-repeat; background-size:400px;">
+				</div>
+			</div>
+			<div class='device-tx1-title darkmode-txt' style="text-align: left !important;">
+				<span>PlayStation 3 w/ CFW or HEN</span>
+			</div>
+			<div class='device-tx2-desc darkmode-txt'>
+				<span>
+				<p>
+					 PlayStation 3 games can be dumped using an original console running a file manager homebrew software such as ManagunZ, IRISMAN or MultiMAN via a custom or hybrid firmware but will require basic knowledge regarding the installation its use.<br>
+					<br>
+					 For more information on this topic, check our <a href='https://wiki.rpcs3.net/index.php?title=Help:Dumping_PlayStation_3_games#How_to_jailbreak_a_PS3_using_PS3HEN' target="_blank">Wiki</a> page.
+				</p>
+				</span>
+			</div>
+		</div>
+	</div>
+	<div class='device-con-outer-right'>
+		<div class='device-con-inner-a'>
+			<div class='performance-con-spec performance-ico-variable'>
+				<div class='performance-ico-spec' style="background: url(/img/icons/buttons/warning-h.png) center no-repeat; background-size:18px;">
+				</div>
+			</div>
+			<div class='device-con-gradient'>
+				<div class='device-con-graphic' style="background: url(/img/graphics/quickstart/bdd.png) center no-repeat; background-size:400px;">
+				</div>
+			</div>
+			<div class='device-tx1-title darkmode-txt' style="text-align: left !important;">
+				<span>PC w/ Blu-ray Drive</span>
+			</div>
+			<div class='device-tx2-desc darkmode-txt'>
+				<span>
+				<p>
+					 PlayStation 3 games can be dumped using a PC with a compatible Blu-ray disc drive using dedicated software but may require advanced knowledge regarding dumping data from game discs. <br>
+					<br>
+					 More information on compatible Blu-ray drives can be found <b>below</b>.
+				</p>
+				</span>
+			</div>
+		</div>
+	</div>
+</div>
